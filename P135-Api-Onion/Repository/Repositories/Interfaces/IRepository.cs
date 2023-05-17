a9be7b9df10efbf29adffed54840d805884d6022
Agshinhummatov <agshinshh@code.edu.az>
2023-05-18 02:07:17 +0400

    API CRUD homework

---
+++ P135-Api-Onion/Repository/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+using Domain.Models;
+using System.Reflection;
+
+namespace Repository.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int? id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<IEnumerable<T>> Search(string name);
+    }
+}
